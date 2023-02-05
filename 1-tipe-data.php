de2f0e0ace0e25da536d5782e43dc15cd5ec64e9
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-02-05 16:19:45 +0700

    memahami tipe data number

--- 1-tipe-data.php
+++ 1-tipe-data.php
@@ -1,4 +1,28 @@
 <?php
-echo "Arfan Salman Ramadhan";
+// tipe data number
+echo "Desimal :";
+var_dump(1234);
+
+echo "Octal :";
+var_dump(01234);
+
+echo "Hexadecimal";
+var_dump(0x1A);
+
+echo "binary";
+var_dump(0b111111);
+
+echo "Underscore in number : ";
+var_dump(1_241_241_241);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point dengan E Notation : (1.7 x 1000) : ";
+var_dump(1.7e3);
+
+echo "Floating Point dengan E Notation minus : (1.7 x 0.001) : ";
+var_dump(1.7e-3);
+
 
 ?>
