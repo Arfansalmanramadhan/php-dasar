d79f09da64d664db5ee6de3f3f209b200f7d4756
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-02-07 16:16:52 +0700

    memmahami tipde data null php

--- 2-variable.php
+++ 2-variable.php
@@ -18,6 +18,28 @@ echo nama;
 echo "<br>";
 echo "Umur : ";
 echo umur;
+echo "<br>";
 
+// data null
+$Nama = "Salman";
+$Nama = NULL;
 
+$Umur = NULL;
+
+echo "Nama : " ;
+echo $Nama;
+echo "<br>";
+
+echo "Umur : ";
+echo $Umur;
+
+echo "<br>";
+echo "Nama null : ";
+var_dump(is_null($Nama));
+echo "<br>";
+$siapa = "Arfan";
+unset($siapa);
+$siapa = "Salman";
+$siapa = NULL;
+var_dump(isset($siapa));
 ?>
