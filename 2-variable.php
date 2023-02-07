8711c71b24b7d9b428a86e400640725938bf43e9
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-02-07 16:02:53 +0700

    Memnmahami variable constant php

--- 2-variable.php
+++ 2-variable.php
@@ -8,7 +8,16 @@ echo $nama;
 echo "<br>";
 echo "Umur : ";
 echo $umur;
+// variable constant
+define("nama", "Arfan Salman Ramadhan");
+define("umur", 20);
 
+echo "<br>";
+echo "Nama : ";
+echo nama;
+echo "<br>";
+echo "Umur : ";
+echo umur;
 
 
 ?>
