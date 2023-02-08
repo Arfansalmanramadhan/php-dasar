63d0d4c4c2f39bd4bf5f9d4957b7e2ebbafc1141
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-02-08 19:51:34 +0700

    memmahami array php

---
+++ 3-array.php
@@ -0,0 +1,10 @@
+<?php 
+$value = array(1,2,3,4,5,6);
+var_dump($value);
+echo "<br>>";
+$nama = ["Arfan", "Salman", "Ramadhan"];
+var_dump($nama);
+echo "<br>";
+$nama[3] = "Azzam";
+var_dump($nama);
+?>
