6aee8d09351e9166e0416e7ff95ca5f896ce85a2
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-02-09 13:35:28 +0700

    memahami array map dan array dalam array

--- 3-array.php
+++ 3-array.php
@@ -7,4 +7,33 @@ var_dump($nama);
 echo "<br>";
 $nama[3] = "Azzam";
 var_dump($nama);
+echo "<br>";
+unset($nama[3]);
+var_dump($nama);
+echo "<br>";
+var_dump(count($nama));
+
+$arfan = array(
+    "id" => "1",
+    "nama" => "Arfan Salman Ramadhan",
+    "umur" => 20,
+    "Kampus" => array(
+        "kuliah" => "Telkom univesirty",
+        "jurusan" => "D3 Teknologi Telekomunikasi",
+        "kelas" => "46-03"
+    )
+    );
+$arfann = [
+    "id" => "1",
+    "nama" => "Arfan Salman Ramadhan",
+    "umur" => 20,
+    "Kampus" => [
+        "kuliah" => "Telkom univesirty",
+        "jurusan" => "D3 Teknologi Telekomunikasi",
+        "kelas" => "46-03"
+    ]
+];
+var_dump($arfan);
+var_dump($arfann);
+
 ?>
