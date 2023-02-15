1a41e19fce31e88a3f4e282d1aec82956990d33c
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-02-15 20:41:28 +0700

    memahami if tenary part 1 php

--- 7-perkondisian.php
+++ 7-perkondisian.php
@@ -16,6 +16,19 @@
     } else {
         echo "Nilai anda E";
     }
+    if($nilai >= 80 && $absen >= 75 ):
+        echo "Nilai anda A";
+     elseif ($nilai >=70 && $absen >= 75) :
+        echo "NIlai anda AB";
+     elseif ($nilai >=65 && $absen >= 75) :
+        echo "NIlai anda B";
+     elseif ($nilai >=60 && $absen >= 75) :
+        echo "Nilai anda BC";
+     elseif ($nilai >=50 && $absen >= 75) :
+        echo "Nilai anda C";
+     else :
+        echo "Nilai anda E";
+     endif;
 
     
 ?>
