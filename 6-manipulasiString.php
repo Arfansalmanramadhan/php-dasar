3f421c5a51059472f5bd03b05608e5f084d9aa7a
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-02-15 19:13:57 +0700

    memahami manipulasi string php

---
+++ 6-manipulasiString.php
@@ -0,0 +1,23 @@
+<?php
+     $nama = "Arfan Salman Ramadhan";
+     echo "nama : " . $nama ;
+     echo "<br>";
+     echo "hasil : " . 50;
+     echo "<br>";
+     $hasilString = (string)100;
+     var_dump($hasilString);
+     $hasilInt = (int)"100";
+     var_dump($hasilInt);
+     $hasilFloat = (float)"100.2";
+     var_dump($hasilFloat);
+
+     $name = "Arfan";
+     echo $name[0];
+     echo $name[1];
+     echo $name[2];
+     echo $name[3];
+     echo"<br>";
+     echo "hello $name, selamat pagi";
+     echo "hello {$name}, selamat pagi"
+
+?>
