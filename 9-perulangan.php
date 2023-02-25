0d645f8920a027d765d5ddfe4b6915f0ac96c530
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-02-25 19:33:54 +0700

    memahami perulangan dengan for dan membuat aplikasi fizzbuzz dengan for di php

---
+++ 9-perulangan.php
@@ -0,0 +1,20 @@
+<?php
+         for ($a = 10; $a >= 1; $a--):
+            echo $a ,"<br>";
+         endfor;
+     for($j = 1; $j <= 10; $j++) {
+        echo $j ,"<br>";
+     }
+    //  echo "<br>";
+     for($b = 1; $b < 21 + 1; $b++):
+        if($b % 3 == 0 && $b % 5 == 0):
+            echo "FizzBuzz <br>";
+        elseif($b % 3 == 0):
+            echo "Fizz" ."<br>";
+        elseif($b % 5 == 0):
+            echo "buzz" ."<br>";
+        else :
+            echo $b , "<br>";
+        endif;
+     endfor;
+?>
