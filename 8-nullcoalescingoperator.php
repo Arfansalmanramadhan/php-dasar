7ee0859c9c3a4b194e845d143701f789d80ac22e
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-02-25 18:52:27 +0700

    memahami null coalescing operator di php

---
+++ 8-nullcoalescingoperator.php
@@ -0,0 +1,11 @@
+<?php
+     $date = [
+        "name" => "Arfan"
+     ];
+    //  $name = null;
+    //  if ($date["name"]){
+    //     $name = "Salman";
+    //  }
+    $name = $data["name"] ?? "Salman";
+     echo $name
+?>
