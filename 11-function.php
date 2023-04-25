c3feb01237df6acec987dcc617d004a1d17e9c15
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-04-25 20:57:05 +0700

    memahami function return

--- 11-function.php
+++ 11-function.php
@@ -37,4 +37,44 @@ function sumAll(...$hasil){
 $hasil = [1,2,3,4,10];
 sumAll(1,2,3,4,5);
 sumAll(...$hasil);
+// function return
+echo "<br>";
+function nilai(int $pertama, int $kedua): int {
+    $total = $pertama + $kedua;
+    return $total;
+}
+var_dump(nilai(10,25));
+echo "<br>";
+function fizzbuzz( $a, $b){
+    for($a = 1; $a < $b + 2; $a++){
+        if($a % 3 == 0 && $a % 5 == 0){
+            echo "Fizzbuzz". "<br>";
+        } else if ($a % 3 == 0){
+            echo "Fizz". "<br>";
+        }else if ($a % 5 == 0) {
+            echo "buzz". "<br>";
+        } else {
+            echo $a. "<br>";
+        }
+    }
+}
+$c = fizzbuzz("total",20);
+echo $c;
+echo "<br>";
+function hasilnilai(int $nilai, int $absen): string {
+    if($nilai >= 80 && $absen >= 75){
+        return "A";
+    } else if ($nilai >= 70 && $absen >= 75){
+        return "AB";
+    } else if ($nilai >= 65 && $absen >= 75){
+        return "B";
+    } else if ($nilai >= 60 && $absen >= 75){
+        return "BC";
+    } else if ($nilai >= 50 && $absen >= 75){
+        return "C";
+    } else {
+        return "E";
+    }
+}
+var_dump(hasilnilai(61,100))
 ?>
