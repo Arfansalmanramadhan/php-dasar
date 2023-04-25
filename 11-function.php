6f9fbfda2b56d2c5284ef323ac3119ad745d0456
Arfansalmanramadhan <arfansalman9@gmail.com>
2023-04-25 16:48:08 +0700

    memahami argument function

--- 11-function.php
+++ 11-function.php
@@ -1,8 +1,40 @@
 <?php
-if (false){
+if (true){
     function say(){
         echo "ARFAN SALMAN RAMADHAN";
     }
 }
-say()
+say();
+// Function Argument
+echo "<br>";
+function nama($name){
+    echo "Halo $name";
+}
+nama("arfan");
+echo "<br>";
+function hal($namade= "Arfan", $namabe="Salman Ramadhan"){
+    echo "halo $namade $namabe";
+}
+hal("Azzam", "Salman H");
+// hal("Salman Ramadhan");
+echo "<br>";
+function sum(int $pertama, int $kedua){
+    $total = $pertama * $kedua;
+    echo "Total $pertama * $kedua = $total";
+}
+sum(5,20);
+sum("5","20");
+sum(true, false );
+echo "<br>";
+function sumAll(...$hasil){
+    $total = 0;
+    foreach ($hasil as $hasill) :
+        $total += $hasill;
+    endforeach;
+    echo "Total ". implode(",", $hasil). "= $total";
+
+}
+$hasil = [1,2,3,4,10];
+sumAll(1,2,3,4,5);
+sumAll(...$hasil);
 ?>
